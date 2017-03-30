ed35f4ca1842831f8351ed9467239ad479e96656
Griko Nibras <grikomsn@gmail.com>
2017-03-30 07:01:59 +0700

    Reformat app.js dan tambah README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# scraping
+Contoh sederhana scraping web [library.itats.ac.id](http://library.itats.ac.id/index.php?search=search&keywords=&page=1). Hasil scraping dapat dilihat pada file [simpan.txt](https://github.com/devitapp-codes/scraping/blob/master/simpan.txt).
+
+Kontributor: [andreiwahyu](https://github.com/andreiwahyu) dan [ok-google](https://github.com/ok-google)
--- app.js
+++ app.js
@@ -3,58 +3,58 @@ let cheerio = require("cheerio"),
     request = require("request"),
     mysql = require('mysql');
 
-let tampung="";
-let start=1;
-let hal=0;
+let tampung = "";
+let start = 1;
+let hal = 0;
 
 let pool = mysql.createPool({
- host     : 'localhost',
- user     : 'root',
- password : '',
- database : 'scraping'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'scraping'
 });
 
-function scrap(url){
-    request(url, function(error, response, html){
+function scrap(url) {
+    request(url, function (error, response, html) {
         hal++;
         let $ = cheerio.load(html);
-        if(!error){
-            let tampil = $('.titleField') ;
-            for (let awal=0; awal<tampil.length; awal++) {  
-                if(awal == 0 && hal==1){
-                    tampung = tampung + start+". "+($(tampil[awal]).text());
-                }
-                else{
-                    tampung = tampung +"\n" + start + ". " + ($(tampil[awal]).text());
+        if (!error) {
+            let tampil = $('.titleField');
+            for (let awal = 0; awal < tampil.length; awal++) {
+                if (awal == 0 && hal == 1) {
+                    tampung = tampung + start + ". " + ($(tampil[awal]).text());
+                } else {
+                    tampung = tampung + "\n" + start + ". " + ($(tampil[awal]).text());
                 }
                 start++;
-                
-                  pool.getConnection(function(err, connection) {
-                    connection.query('INSERT INTO hasil SET ?', {judul: $(tampil[awal]).text()}, function(err, result) {
+
+                pool.getConnection(function (err, connection) {
+                    connection.query('INSERT INTO hasil SET ?', {
+                        judul: $(tampil[awal]).text()
+                    }, function (err, result) {
                         if (err) throw err;
                         console.log(result.insertId);
                     });
                     connection.release();
-                  });
+                });
             }
         }
-        
-        fs.writeFile("simpan.txt", tampung, function(err){
-               if(err){
-                   return console.log(err);
-               } 
-            });
-        
-    if(hal == 331){
-        console.log("Scrapping End");
-        console.log("File Tersimpan");
-    }
-    else{
-        let next = $('.next_link')[0].attribs.href;
-        scrap('http://library.itats.ac.id'+next);
-    }
+
+        fs.writeFile("simpan.txt", tampung, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+
+        if (hal == 331) {
+            console.log("Scrapping End");
+            console.log("File Tersimpan");
+        } else {
+            let next = $('.next_link')[0].attribs.href;
+            scrap('http://library.itats.ac.id' + next);
+        }
     });
 }
 
-let urls='http://library.itats.ac.id/index.php?search=search&keywords=&page=1';
+let urls = 'http://library.itats.ac.id/index.php?search=search&keywords=&page=1';
 scrap(urls);
